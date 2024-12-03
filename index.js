4fa7c7d93a52c383083360d0297abea5e259f275
Kibet Kipsang <denniskipsang2000@gmail.com>
2024-12-03 23:15:33 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat (Ambuso){
+      cats.push(Ambuso);
+}
+function destructivelyPrependCat(Carlos){
+      cats.unshift(Carlos);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function  appendCat(Lyon){
+const othercats = cats.slice();
+      othercats.push (Lyon);
+      return othercats;
+}
+
+function  prependCat(Buddy){
+    const simplecats =cats.slice();
+          simplecats.unshift (Buddy);
+          return simplecats;
+}
+function   removeLastCat(){
+    const ourcats =cats.slice();
+   ourcats.pop ();
+    return ourcats;
+}
+function removeFirstCat(){
+    const freshCats = cats.slice(1);
+    return freshCats;
+}
